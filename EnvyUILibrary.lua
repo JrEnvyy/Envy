189dc6ee38a5806b4420dd329afd7fe023b2f479
JrEnvyy <99378912+JrEnvyy@users.noreply.github.com>
2022-02-15 17:24:33 +0800

    Update EnvyUILibrary.lua

--- EnvyUILibrary.lua
+++ EnvyUILibrary.lua
@@ -14,11 +14,11 @@ local utility = {}
 -- themes
 local objects = {}
 local themes = {
-	Background = Color3.fromRGB(4, 117, 158),
+	Background = Color3.fromRGB(64, 64, 64),
 	Glow = Color3.fromRGB(255, 255, 255),
 	Accent = Color3.fromRGB(10, 148, 198),
-	LightContrast = Color3.fromRGB(21, 115, 149),
-	DarkContrast = Color3.fromRGB(0, 51, 102),
+	LightContrast = Color3.fromRGB(64, 64, 64),
+	DarkContrast = Color3.fromRGB(64, 64, 64),
 	TextColor = Color3.fromRGB(255, 255, 255)
 }
 
